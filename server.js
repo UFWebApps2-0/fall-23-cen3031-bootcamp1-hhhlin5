fd6f529270c0309209c1629df62ec3305e1e90a5
hailinz <h.zeng@ufl.edu>
2023-09-11 15:13:48 -0400

    http server has been create

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var requestHandler = function(request, response) {
   /*Investigate the request object. 
     You will need to use several of its properties: url and method
   */
-  //console.log(request);
+  console.log(request);
 
   /*
     Your request handler should send listingData in the JSON format as a response if a GET request 
@@ -28,7 +28,19 @@ var requestHandler = function(request, response) {
     HINT: Explore mdn web docs for resources on how to use javascript.
     Helpful example: if-else structure- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
 
-    */
+  */
+  
+  // check if it is a GET request and is sent to the "/listings" path
+  if (request.method === 'GET' && request.url === '/listings'){
+    // send listingData in the JSON format as a response
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(JSON.stringify(listingData)); // The JSON.stringify() static method converts a JavaScript value to a JSON string
+  }
+  else {
+    // Send a 404 error response for all other requests
+    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.end('404 Not Found');
+  }
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
@@ -43,18 +55,34 @@ fs.readFile('listings.json', 'utf8', function(err, data) {
     http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
    */
 
-    //Check for errors
+  //Check for errors
+  if (err) {
+    console.error(err);
+    return
+  }
+  // Parse the JSON data and save it in the listingData variable
+  try {
+    listingData = JSON.parse(data);
+    // output the parsed data
+    console.log(listingData);
+  } 
+  catch (err) {
+    console.error('Error while parsing JSON data:', err);
+  }
+
     /*this resource gives you an idea of the general format err objects and Throwing an existing object.
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw#throwing_an_existing_object
    */
   
 
    //Save the data in the listingData variable already defined
+
+  //Creates the server, but not started 
+  var server = http.createServer(requestHandler);
   
-
-  //Creates the server
-  
-  //Start the server
-
-
+  //Start the server, listening for request on port 8080
+  server.listen(port,function(){
+    // once the server is listening, this callback function is executed
+    console.log('Server listening on:  http://localhost:' + port);
+  })
 });
